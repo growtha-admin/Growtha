dddbf62f8a2c93969ab126c5f80be429a6f2edc9
Stevens Reality <stevensreality@outlook.com>
2023-09-14 14:29:21 +0100

    update changes

--- productDisplay.js
+++ productDisplay.js
@@ -13,119 +13,41 @@ let productDisplaydata
 
 
 fetch('products.json')
-    .then(response => response.json())
-    .then(data => {
-        productDisplaydata = data;
+.then(response => response.json())
+.then(data => {
+    productDisplaydata = data;
 
-        data.forEach(product => {
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product');
+    data.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
 
-            productDiv.innerHTML =
-                `
-                <div class="product_content">
-                    <div class="product_image_div">
-                        <img class="product_image" src="${product.image}" alt="${product.imagealttext}">
+        productDiv.innerHTML =
+            `
+            <div class="product_content">
+                <div class="product_image_div">
+                    <img class="product_image" src="${product.image}" alt="${product.imagealttext}">
+                </div>
+                <div class="product_content_text">
+                    <span class="product_type">${product.type}</span>
+                    <h3 class="product_title">${product.title}</h3>
+                    <p class="creator_name_wrapper">
+                        By
+                        <strong class="creator_name">${product.creatorname}</strong>
+                    </p>
+                    <p class="product_desc">${product.description}</p>
+                    <div class="product_details">
+                        <p class="price">${product.price}</p>
+                        <a href="${product.url}" target="_blank" rel="noopener noreferrer" class="btn product_homebtn" id="product_homebtn">View Details</a>
                     </div>
-                    <div class="product_content_text">
-                        <span class="product_type">${product.type}</span>
-                        <h3 class="product_title">${product.title}</h3>
-                        <p class="creator_name_wrapper">
-                            By
-                            <strong class="creator_name">${product.creatorname}</strong>
-                        </p>
-                        <p class="product_desc">${product.description}</p>
-                        <div class="product_details">
-                            <p class="price">${product.price}</p>
-                            <a href="${product.url}" target="_blank" rel="noopener noreferrer" class="btn product_homebtn" id="product_homebtn">View Details</a>
-                        </div>
-                    </div>
-                `
-            ;
+                </div>
+            `
+        ;
 
-            productDisplay.appendChild(productDiv);
-        });
-
-
-        
-        
-    })
-    .catch(error => {
-        console.log('Error:', error);
-        productDisplay.innerHTML = 'Error: ' + error;
+        productDisplay.appendChild(productDiv);
     });
-
-
-function displayproductDialog() {
-    openproductDialog.addEventListener('click', (e) => {
-        e.preventDefault()
-
-        console.log(e)
-    
-        productDialog.showModal()
-        document.body.classList.add('station_body')
-        overlay.style.display('block')                                    
-    })
-    
-    closeproductDialog.addEventListener('click', () => {
-        productDialog.close();
-        document.body.classList.remove('station_body')
-        overlay.style.display = 'none'  
-    })
-}
-
-// openproductDialog.forEach((container) => {
-//     console.log(container)
-//     container.addEventListener('click', () => {
-//         if (productDisplaydata){
-//             const { type, description } = product
-//         }
-//     })
-// })
-
-
-// ProductQueryParameterMatch();
-
-// displayproductDialog()
-
-// productDialogContent()
-
-// window.addEventListener('load', openproductDialogIQPM)
-
-
-function ProductQueryParameterMatch() {
-    const urlParam = new URLSearchParams(window.location.search);
-    console.log(urlParam)
-    const productParam = urlParam.get('product');
-    console.log(productParam)
-
-    if (productParam) {
-        fetch('products.json')
-        .then(response => response.json())
-        .then(data => {
-            
-            
-            const productDesc = document.getAnimations('product_desc')
-            productDesc.textContent = JSON.stringify(data)
-
-
-    
-            console.log(productDesc.textContent)
-
-
-            productDialog.showModal()
-        })
-        .catch(error => {
-            console.log('Wahala', error)
-        })
-    } else {
-        console.log('Query error')
-    }
-}
-
-openproductDialog.addEventListener('click', (e) => {
-    e.preventDefault()
-
-
+        
 })
-
+.catch(error => {
+    console.log('Error:', error);
+    productDisplay.innerHTML = 'Error: ' + error;
+});
